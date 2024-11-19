1a322597d87835ea5484e8c6fb12f552d875ab0a
boldi-kdg <boldizsar.olajos@student.kdg.be>
2024-11-19 07:11:00 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    `java-library`
+}
+
+group = "be.kdg.int5"
+version = "1.0.0"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.1")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
