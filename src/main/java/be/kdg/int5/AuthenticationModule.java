6cbfb51600f938e34d80c0b7251a31712dfe53c8
boldi-kdg <boldizsar.olajos@student.kdg.be>
2024-11-21 15:46:17 +0100

    Authentication: add informative exception messages

--- src/main/java/be/kdg/int5/AuthenticationModule.java
+++ src/main/java/be/kdg/int5/AuthenticationModule.java
@@ -20,19 +20,19 @@ class AuthenticationModule {
         try {
             HttpResponse<String> response = sdk.httpClient.send(request, HttpResponse.BodyHandlers.ofString());
             if(response.statusCode() != 200) {
-                throw new GameSDK.AuthenticationFailedException();
+                throw new GameSDK.AuthenticationFailedException("Non-OK response status code: "+response.statusCode());
             }
 
             @SuppressWarnings("unchecked")
             Map<String, Object> jsonMap = sdk.objectMapper.readValue(response.body(), Map.class);
             if(!jsonMap.containsKey("access_token") || !jsonMap.containsKey("expires_in")) {
-                throw new GameSDK.AuthenticationFailedException();
+                throw new GameSDK.AuthenticationFailedException("Got malformed response");
             }
 
             sdk.bearerToken = (String) jsonMap.get("access_token");
             sdk.tokenExpiresAt = Instant.now().plusSeconds(((int) jsonMap.get("expires_in")) - sdk.tokenExpirationMargin);
         }catch (IOException | InterruptedException e) {
-            throw new GameSDK.AuthenticationFailedException();
+            throw new GameSDK.AuthenticationFailedException(e.getMessage());
         }
     }
 }
